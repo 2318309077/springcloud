8829ff68d4fd4a64b117e7403f2220f6eba6a343
wujinliang <2318309077@qq.com>
2019-08-19 15:48:48 +0800

    15点48分

--- main-esAndCanal/src/main/java/com/ceway/esAndCanal/canal/www.java
+++
@@ -1,9 +0,0 @@
-package com.ceway.esAndCanal.canal;
-
-public class www {
-
-	public static void main(String[] args) {
-		ClientSample.woke();
-	}
-
-}
--- main-esAndCanal/src/main/java/com/ceway/esAndCanal/controller/work.java
+++ main-esAndCanal/src/main/java/com/ceway/esAndCanal/controller/work.java
@@ -1,5 +1,6 @@
 package com.ceway.esAndCanal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/add/")
 public class work {
-       @RequestMapping("/sj")
+	   
+	      @RequestMapping("/sj")
        public String sd(){
-    	   System.out.println("===================="+System.currentTimeMillis());
+    	  System.out.println("===================="+System.currentTimeMillis());
     	   return "伍金良的路由!";
        }
 }
